288b1d07c4a22616ce7c9add4e0b772a4e12a610
Simon Boehm <simon@siboehm.com>
2022-09-01 08:50:08 +0200

    PyTorch DDP: Fix dimensions, make MPI in place

--- DDP_MNIST_MLP.py
+++ DDP_MNIST_MLP.py
@@ -14,11 +14,11 @@ torch.manual_seed(0)
 
 # Define an MLP classifier
 class MLP(nn.Module):
-    def __init__(self, input_size: int, hidden_sizes: list[int], output_size: int):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_sizes[0])
-        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
-        self.fc3 = nn.Linear(hidden_sizes[1], output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     )
 
     # define the model
-    model = MLP(input_size=28 * 28, hidden_size=[64, 64], output_size=10)
+    model = MLP(input_size=28 * 28, hidden_size=64, output_size=10)
     # make sure the initialization is the same on all processes
     assert_sync(get_model_hash(model))
 
@@ -98,9 +98,12 @@ if __name__ == "__main__":
     for epoch in range(NUM_EPOCHS):
         epoch_start_time = time.time()
         for i, (images, labels) in enumerate(train_loader):
-            images = images.view(-1, 1, 28, 28)
+            images = images.view(-1, 28 * 28)
             outputs = model(images)
             loss = criterion(outputs, labels)
+            # rescale the loss to be a mean over the global batch size instead of
+            # just the local batch size (= global batch size / comm.size, see above)
+            loss /= comm.size
             optimizer.zero_grad()
             # compute the gradients locally
             loss.backward()
@@ -109,9 +112,7 @@ if __name__ == "__main__":
                 # todo: gather the activations instead of gradients
                 # todo: do this in a single communication step by allocating a large tensor
                 for param in model.parameters():
-                    recv = torch.zeros_like(param.grad)
-                    comm.Allreduce(param.grad / size, recv, op=MPI.SUM)
-                    param.grad = recv
+                    comm.Allreduce(MPI.IN_PLACE, param.grad, op=MPI.SUM)
 
             optimizer.step()
             if (i + 1) % 100 == 0:
@@ -121,7 +122,7 @@ if __name__ == "__main__":
                         NUM_EPOCHS,
                         i + 1,
                         len(train_loader),
-                        loss.item(),
+                        loss.item() * 4,
                     )
                 )
         rprint("Time(epoch) {:4.1f}s".format(time.time() - epoch_start_time))
@@ -132,7 +133,7 @@ if __name__ == "__main__":
         correct = 0
         total = 0
         for images, labels in test_loader:
-            images = images.view(-1, 1, 28, 28)
+            images = images.view(-1, 28 * 28)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
@@ -146,13 +147,14 @@ if __name__ == "__main__":
     # make sure the final model is the same on all processes
     assert_sync(get_model_hash(model))
 
-    if False:
-        torch.save(model.state_dict(), f"data/models/model_p{size}.pkl")
+    torch.save(model.state_dict(), f"data/models/model_p{size}.pkl")
 
-        # compare the absolute divergence between the two models
-        sequential_model = MLP(input_size=28 * 28, hidden_size=64, output_size=10)
-        sequential_model.load_state_dict(torch.load(f"data/models/model_p1.pkl"))
-        divergence = 0
-        for param1, param2 in zip(model.parameters(), sequential_model.parameters()):
-            divergence += torch.abs(param1 - param2).sum().item()
-        rprint("Absolute divergence cmp to serial weights: {:.8f}".format(divergence))
+    # compare the absolute divergence between the two models
+    sequential_model = MLP(input_size=28 * 28, hidden_size=64, output_size=10)
+    sequential_model.load_state_dict(torch.load(f"data/models/model_p1.pkl"))
+    divergence = 0
+    for param1, param2 in zip(model.parameters(), sequential_model.parameters()):
+        divergence += torch.abs(param1 - param2).sum().item()
+    rprint(
+        "Total absolute divergence cmp'd to serial weights: {:.8f}".format(divergence)
+    )
